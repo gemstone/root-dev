55084b802beeafdee016a600c28d5870bb283d8b
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-12-19 10:08:05 -0500

    Added clone-all script

---
+++ clone-all.cmd
@@ -0,0 +1,9 @@
+@echo off
+cd ..
+git clone https://github.com/gemstone/shared-content.git
+git clone https://github.com/gemstone/gemtem.git
+git clone https://github.com/gemstone/common.git
+git clone https://github.com/gemstone/threading.git
+git clone https://github.com/gemstone/expressions.git
+git clone https://github.com/gemstone/data.git
+git clone https://github.com/gemstone/pqdif.git
