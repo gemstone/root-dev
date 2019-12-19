d75a2dc188a92e78e58fbca68cc774f005b9d12c
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-12-19 12:05:02 -0500

    Added gemstone.io to clone-all script

--- clone-all.cmd
+++ clone-all.cmd
@@ -4,6 +4,8 @@ git clone https://github.com/gemstone/shared-content.git
 git clone https://github.com/gemstone/gemtem.git
 git clone https://github.com/gemstone/common.git
 git clone https://github.com/gemstone/threading.git
+git clone https://github.com/gemstone/io.git
 git clone https://github.com/gemstone/expressions.git
 git clone https://github.com/gemstone/data.git
 git clone https://github.com/gemstone/pqdif.git
+cd root-dev
