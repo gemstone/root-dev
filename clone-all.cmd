6dda42c340038e41eda207870faeefb5b58c4f06
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-12-21 01:46:02 -0500

    Added data project to solution

--- clone-all.cmd
+++ clone-all.cmd
@@ -3,6 +3,7 @@ cd ..
 git clone https://github.com/gemstone/shared-content.git
 git clone https://github.com/gemstone/gemtem.git
 git clone https://github.com/gemstone/common.git
+git clone https://github.com/gemstone/numeric.git
 git clone https://github.com/gemstone/threading.git
 git clone https://github.com/gemstone/io.git
 git clone https://github.com/gemstone/expressions.git
